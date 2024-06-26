bbb1485ff28d3257ea4b0fa033d54e0eab5a1063
Soo <66674138+KSAhh@users.noreply.github.com>
2024-06-26 11:35:37 +0900

    Create 01 basic.py

---
+++ Python/Jump to python/01 basic.py
@@ -0,0 +1,35 @@
+"""
+컴파일 언어
+- 기계어로 변환  
+- C언어  
+
+인터프리터 언어  
+- python 
+
+---
+
+웹 프로그래밍
+- Django / FastAPI / Flask
+인공지능과 머신러닝
+- Tensorflow / PyTorch
+수치 연산 프로그래밍
+- NumPy
+데이터 분석
+- pandas
+
+데이터베이스 프로그래밍
+시스템 유틸리티 제작
+GUI 프로그래밍
+C/C++과 결합
+IoT
+
+불가
+- 모바일 프로그래밍
+- 시스템과 밀접한 프로그래밍 (JAVASCRIPT - ReactNative / Dart-Flutter)
+
+3.11.2ver.
+Download Windows installer (64-bit)
+
+>>>
+REPL (Read Evaluate Print Loop) : 파이썬 구문 입력하고 결과 볼 수 있는 도구
+"""
